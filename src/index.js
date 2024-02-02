6f673c5b0ea5f728e4d95179a373d42ece266b66
Vidit Purohit <vidit01purohit@gmail.com>
2024-02-02 23:54:33 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="vidit.jpeg" alt="Vidit Purohit" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Vidit Purohit</h1>
+      <p>
+        Full-stack web developer and AI/ML Engineer. When not coding or
+        preparing a course, I like to play chess, badminton, basketball or
+        cricket or sometimes just binge watching.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Python" emoji="💪" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
